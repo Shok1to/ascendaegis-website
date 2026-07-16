36c49d9af7a92c8362f23556ade1395917752316
Shok1to <shoko.takahashi.911@gmail.com>
2026-07-16 17:09:50 -0400

    Adjust logo

--- components/shield-logo.tsx
+++ components/shield-logo.tsx
@@ -6,9 +6,9 @@ export function ShieldLogo({ className }: { className?: string }) {
     <Image
       src="/images/logo.svg"
       alt="Ascend Aegis"
-      width={559}
+      width={800}
       height={250}
-      className={cn("h-10 w-auto object-contain", className)}
+      className={cn("h-14 w-auto object-contain", className)}
     />
   )
 }
