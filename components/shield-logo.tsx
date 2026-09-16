6e2ce5538142d25c2ea8a52abd1724c5210c08ac
Shok1to <shoko.takahashi.911@gmail.com>
2026-09-15 22:51:08 -0400

    changed logo size

--- components/shield-logo.tsx
+++ components/shield-logo.tsx
@@ -8,7 +8,7 @@ export function ShieldLogo({ className }: { className?: string }) {
       alt="Ascend Aegis"
       width={100}
       height={100}
-      className={cn("h-16 w-16 object-contain", className)}
+      className={cn("h-20 w-20 object-contain", className)}
     />
   )
 }
