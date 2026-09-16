0f14072a2f8e948cefdc6c2ae4cd9fc2d386b2d9
Shok1to <shoko.takahashi.911@gmail.com>
2026-09-15 22:43:53 -0400

    changed png tp svg file

--- components/shield-logo.tsx
+++ components/shield-logo.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils"
 export function ShieldLogo({ className }: { className?: string }) {
   return (
     <Image
-      src="/images/logo.png"
+      src="/images/logo.svg"
       alt="Ascend Aegis"
       width={80}
       height={80}
